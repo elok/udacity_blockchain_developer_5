0247e418e2093988b8d182106fbb6e37b9ddeb4b
Eric Lok <ericlok@gmail.com>
2022-06-13 22:13:29 -0400

    adding truffle config and deployment scripts

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -2,7 +2,27 @@
 var SquareVerifier = artifacts.require("./SquareVerifier.sol");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SquareVerifier);
-  deployer.deploy(SolnSquareVerifier);
-};
+// module.exports = function (deployer) {
+//   deployer.deploy(Verifier);
+//   deployer.deploy(SolnSquareVerifier);
+// };
+
+module.exports = function (deployer) {
+  deployer.deploy(SquareVerifier)
+    .then(() => {
+      return deployer.deploy(SolnSquareVerifier, SquareVerifier.address)
+    });
+}
+
+// migrating the appropriate contracts
+// const LokToken = artifacts.require("LokToken");
+// const SquareVerifier = artifacts.require("SquareVerifier");
+// const SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+
+// module.exports = function(deployer, network) {
+//   if (!(['mainnet', 'rinkeby'].includes(network))) {
+//     deployer.deploy(LokToken);
+//   }
+//   deployer.deploy(SquareVerifier)
+//     .then(() => deployer.deploy(SolnSquareVerifier, SquareVerifier.address));
+// };
